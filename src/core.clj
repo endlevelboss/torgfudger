ee1be6a05a160f8da7d2496192cf6ecd74d6c749
Tony A. Ingebrigtsen <16385316+endlevelboss@users.noreply.github.com>
2025-03-21 13:52:58 +0100

    mothership

--- src/core.clj
+++ src/core.clj
@@ -172,6 +172,104 @@
   {:name name :description description :Tech tech :Bonus bonus :Price price :Notes notes})
 
 
+
+(defn weapon [[name description cost range damage shots wound special]]
+  {:type "Weapons" :name name :description description
+   :rulesets [{:system "mothership" 
+               :implementation 
+               {:weaponStats 
+                {:Stats {:Cost cost
+                         :Range range
+                         :Damage damage
+                         :Shots shots
+                         :Wound wound
+                         :Special special}}}}]})
+
+(def weapons [["Boarding Axe" "" "150cr" "Adjacent" "2d10" "N/A" "Gore [+]" ""]
+              ["Combat Shotgun" "" "1,400cr" "Close" "4d10" "4" "Gunshot" "1d10 DMG at Long Range or greater"]
+              ["Crowbar" "" "25cr" "Adjacent" "1d5" "N/A" "Blunt Force [+]" "Grants [+] on Strength Checks to open jammed airlocks, lift heavy objects, etc."]
+              ["Flamethrower" "" "4kcr" "Close" "2d10" "4" "Fire/Explosives [+]" "Body Save [-] or be set of fire (2d10 DMG / round)"]
+              ["Flare Gun" "" "25cr" "Long" "1d5" "2" "Fire/Explosives [-]" "High intensity flare visible day and night from Long Range"]
+              ["Foam Gun" "" "500cr" "Close" "1" "3" "Blunt Force" "Body Save or become stuck. Strength Check [-] to escape"]
+              ["Frag Grenade" "" "400cr" "Close" "3d10" "1" "Fire/Explosive" "On a hit, damages all Adjacent to enemy"]
+              ["General-Purpose Machine Gun" "" "450cr" "Long" "4d10" "5" "Gunshot [+]" "Two-handed. Heavy. Barrel can be maneuvered to fire around corners"]
+              ["Hand Welder" "" "250cr" "Adjacent" "1d10" "N/A" "Bleeding" "Can cut through airlock doors"]
+              ["Laser Cutter" "" "1,200cr" "Long" "1d100" "6" "Bleeding [+] or Gore [+]" "Two-handed. Heavy. 1 round recharge between shots."]
+              ["Nail Gun" "" "150cr" "Close" "1d5" "32" "Bleeding" ""]
+              ["Pulse Rifle" "" "2.4kcr" "Long" "3d10" "5" "Gunshot" ""]
+              ["Revolver" "" "750cr" "Close" "1d10+1" "6" "Gunshot" ""]
+              ["Rigging Gun" "" "350cr" "Close" "1d10 DMG + 2d10 DMG when removed" "1" "Bleeding [+]" "100m micro-filament. Body Save or become entangled."]
+              ["Scalpel" "" "50cr" "Adjacent" "1d5" "N/A" "Bleeding [+]" ""]
+              ["Smart Rifle" "" "5kcr" "Extreme" "4d10 (AA)" "3" "Gunshot [+]" "[-] on Combat Check when fired at Close Range"]
+              ["SMG" "" "1kcr" "Long" "2d10" "5" "Gunshot" "Can be fired one-handed"]
+              ["Stun Baton" "" "150cr" "Adjacent" "1d5" "N/A" "Blunt Force" "Body Save or stunned 1 round"]
+              ["Tranq Pistol" "" "250cr" "Close" "1d5" "6" "Blunt Force" "If DMG dealt: enemy must Body Save or be unconscious 1d10 rounds"]
+              ["Unarmed" "" "Free" "Adjacent" "Str/10 DMG" "N/A" "Blunt Force" ""]
+              ["Vibechete" "" "1kcr" "Adjacent" "3d10 (AA)" "N/A" "Bleeding + Gore" "When dealing a Wound, roll on BOTH the Bleeding AND Gore columns."]])
+
+(defn trait [[name description adjust trauma skills]]
+  {:type "Traits" :name name :description description
+   :rulesets [{:system "mothership"
+               :implementation
+               {:traitStats
+                {:Stats {:Adjust adjust
+                         :Skills skills
+                         "Trauma response" trauma}}}}]})
+
+(def traits [["Marine" 
+              "are handy in a fight, but whenever they Panic it may cause problems for the rest of the crew."
+              "+10 combat, +10 body save, +20 fear save, +1 max wounds"
+              "Whenever you panic, every close friendly player must make a fear save."
+              "Military training, Athletics. Bonus: 1 Expert Skill OR 2 Trained Skills."]
+             ["Android"
+              "are a terrifying and exciting addition to any crew. They tend to unnerve other crewmembers with their cold inhumanity."
+              "+20 intellect, -10 to one stat, +60 fear save, +1 max wounds"
+              "Fear saves made by close friendly players are at a disadvantage"
+              "Linguistic, Computers, Mathematics. Bonus: 1 Expert Skill OR 2 Trained Skills."]
+             ["Scientist"
+              "are doctors, researchers, or anyone who wants to slice open creatures (or infected crewmembers) with a scalpel."
+              "+10 intellect, +5 to one stat, +30 sanity save"
+              "Whenever you fail a sanity save, all close friendly players gain 1 stress"
+              "1 Master Skill, and an Expert and Trained Skill prerequisite. Bonus: 1 Trained Skill"]
+             ["Teamster"
+              "are rough and tumble blue-collar space workers, mechanics, engineers, miners, and pilots"
+              "+5 to all stats, +10 to all saves"
+              "Once per session, you may take advantage on a panic check"
+              "Industrial Equipment, Zero-G. Bonus: 1 Trained Skill and 1 Expert Skill"]])
+
+(defn armor [[name description cost ap o2 speed special]]
+  {:type "Armor" :name name :description description
+   :rulesets [{:system "mothership"
+               :implementation
+               {:armorStats
+                {:Stats {:Cost cost
+                         :AP ap
+                         :O2 o2
+                         :Speed speed
+                         :Special special}}}}]})
+
+(def armors [["Vaccsuit" "Designed for outer space operation." "10kcr" 3 "12 hrs" "[-]" "Includes short-range comms, headlamp, and radiation shielding. Decompression within 1d5 rounds if punctured."]
+             ["Standard Crew Attire" "Basic clothing." "100cr" 1 "None" "Normal" ""]
+             ["Hazard Suit" "Environmental protection while exploring unknown planets." "4kcr" 5 "1 hr" "Normal" "Includes air filter, extreme heat/cold protection,hydration reclamation (1 L of water lasts 4 days), short-range comms, headlamp, and radiation shielding."]
+             ["Standard Battle Dress" "Lightly-plated armor worn by most marines." "2kcr" 7 "None" "Normal" "Includes short-range comms."]
+             ["Advanced Battle Dress" "Heavy armor for marines deployed in high combat offworld engagements." "12kcr" 10 "1 hr" "[-]" "Includes short-range comms, body cam, headlamp, HUD, exoskeletal weave (Strength Checks [+]), and radiation shielding. Damage Reduction: 3"]])
+
+(defn mothershipstuff []
+  (let [
+        ;; weap (reduce #(conj %1 (weapon %2)) [] weapons)
+        trai (reduce #(conj %1 (trait %2)) [] traits)
+        ;; armo (reduce #(conj %1 (armor %2)) [] armors)
+        mothership {:name "Mothership traits"
+                    :description "Traits for mothership"
+                    :entities trai}]
+    (->> mothership
+         (json/write-str)
+         (spit "morskap.json"))))
+
+(mothershipstuff)
+
+
+
 (comment
   (->> (map create-armor d/core-armor)
        (into [])
